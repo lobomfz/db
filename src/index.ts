96cc55076310040b3e595c8f7e42987894af18aa
Lobo <lobomfz@protonmail.com>
2026-03-31 11:07:43 -0300

    export kysely type

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export { Database } from "./database";
 export { generated, type GeneratedPreset } from "./generated";
 export { JsonParseError } from "./errors";
-export { sql, type Selectable, type Insertable, type Updateable } from "kysely";
+export { sql, type Selectable, type Insertable, type Updateable, type Kysely } from "kysely";
 export { type } from "arktype";
 export { JsonValidationError } from "./validation-error";
 export type { DbFieldMeta } from "./env";
